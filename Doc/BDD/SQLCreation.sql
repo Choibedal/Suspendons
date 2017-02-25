e9c2af637f647e36db9c1fbb9bdcf2a36a8d2188
KOOPA\KooPa <clement.vachet@viacesi.fr>
2017-02-25 16:30:22 +0100

    Add Photos with Luc Arbogast
    Make som files to clean up the mess

---
+++ Doc/BDD/SQLCreation.sql
@@ -0,0 +1,153 @@
+/*==============================================================*/
+/* Nom de SGBD :  MySQL 5.0                                     */
+/* Date de création :  13/01/2017 13:15:19                      */
+/*==============================================================*/
+
+
+drop table if exists BENEFICIAIRE;
+
+drop table if exists CAGNOTTE;
+
+drop table if exists REGION;
+
+drop table if exists TRANSACTION_ENTRANTE;
+
+drop table if exists TRANSACTION_SORTANTE;
+
+drop table if exists TYPECAGNOTTE;
+
+drop table if exists TYPEUSER;
+
+drop table if exists UTILISATEUR;
+
+/*==============================================================*/
+/* Table : BENEFICIAIRE                                         */
+/*==============================================================*/
+create table BENEFICIAIRE
+(
+   ID                   numeric not null,
+   IDENTIFIANT          numeric,
+   PRENOM               text,
+   DATECREATION         datetime,
+   DATEMODIFICATION     datetime,
+   DATESUPPRESSION      datetime,
+   IDREGION             numeric,
+   primary key (ID)
+);
+
+/*==============================================================*/
+/* Table : CAGNOTTE                                             */
+/*==============================================================*/
+create table CAGNOTTE
+(
+   ID                   numeric not null,
+   VALEUR               numeric not null,
+   IDREGION             numeric,
+   IDTYPE               numeric,
+   primary key (ID)
+);
+
+/*==============================================================*/
+/* Table : REGION                                               */
+/*==============================================================*/
+create table REGION
+(
+   ID                   numeric not null,
+   NOM                  text not null,
+   primary key (ID)
+);
+
+/*==============================================================*/
+/* Table : TRANSACTION_ENTRANTE                                 */
+/*==============================================================*/
+create table TRANSACTION_ENTRANTE
+(
+   ID                   numeric not null,
+   MONTANT              float(0) not null,
+   DATE                 datetime not null,
+   IDCAGNOTTE           numeric,
+   IDUSER               numeric,
+   primary key (ID)
+);
+
+/*==============================================================*/
+/* Table : TRANSACTION_SORTANTE                                 */
+/*==============================================================*/
+create table TRANSACTION_SORTANTE
+(
+   ID                   numeric not null,
+   MONTANT              float(0),
+   DATE                 datetime,
+   IDCAGNOTTE           numeric,
+   IDBENEFICIAIRE       numeric,
+   IDPARTENAIRE         numeric,
+   primary key (ID)
+);
+
+/*==============================================================*/
+/* Table : TYPECAGNOTTE                                         */
+/*==============================================================*/
+create table TYPECAGNOTTE
+(
+   ID                   numeric not null,
+   NOM                  text not null,
+   primary key (ID)
+);
+
+/*==============================================================*/
+/* Table : TYPEUSER                                             */
+/*==============================================================*/
+create table TYPEUSER
+(
+   ID                   numeric not null,
+   NOM                  text not null,
+   primary key (ID)
+);
+
+/*==============================================================*/
+/* Table : UTILISATEUR                                          */
+/*==============================================================*/
+create table UTILISATEUR
+(
+   ID                   numeric not null,
+   PSEUDO               text,
+   MAIL                 text,
+   MDP                  text,
+   ADRESSE              text,
+   CP                   int,
+   VILLE                text,
+   DATECREATION         datetime,
+   DATEMODIFICATION     datetime,
+   DATESUPPRESSION      datetime,
+   IDTYPE               numeric,
+   IDREGION             numeric,
+   primary key (ID)
+);
+
+alter table BENEFICIAIRE add constraint FK_REFERENCE_2 foreign key (IDREGION)
+      references REGION (ID) on delete restrict on update restrict;
+
+alter table CAGNOTTE add constraint FK_REFERENCE_4 foreign key (IDREGION)
+      references REGION (ID) on delete restrict on update restrict;
+
+alter table CAGNOTTE add constraint FK_REFERENCE_5 foreign key (IDTYPE)
+      references TYPECAGNOTTE (ID) on delete restrict on update restrict;
+
+alter table TRANSACTION_ENTRANTE add constraint FK_REFERENCE_7 foreign key (IDCAGNOTTE)
+      references CAGNOTTE (ID) on delete restrict on update restrict;
+
+alter table TRANSACTION_ENTRANTE add constraint FK_REFERENCE_8 foreign key (IDUSER)
+      references UTILISATEUR (ID) on delete restrict on update restrict;
+
+alter table TRANSACTION_SORTANTE add constraint FK_REFERENCE_3 foreign key (ID)
+      references BENEFICIAIRE (ID) on delete restrict on update restrict;
+
+alter table TRANSACTION_SORTANTE add constraint FK_REFERENCE_6 foreign key (IDCAGNOTTE)
+      references CAGNOTTE (ID) on delete restrict on update restrict;
+
+alter table UTILISATEUR add constraint FK_REFERENCE_10 foreign key (IDREGION)
+      references REGION (ID) on delete restrict on update restrict;
+
+alter table UTILISATEUR add constraint FK_REFERENCE_9 foreign key (IDTYPE)
+      references TYPEUSER (ID) on delete restrict on update restrict;
+
---
+++ Doc/BDD/SQLDummyData.sql
@@ -0,0 +1,17 @@
+INSERT INTO `utilisateur` (`ID`, `PSEUDO`, `MAIL`, `MDP`, `ADRESSE`, `CP`, `VILLE`, `DATECREATION`, `DATEMODIFICATION`, `DATESUPPRESSION`, `IDTYPE`, `IDREGION`) VALUES
+('2', 'Restaurant Le Kuhn', 'contact@lekuhn.com', 'notAPassword', '15 Rue Kuhn', 67000, 'Strasbourg', '2017-02-10 00:00:00', NULL, NULL, '1', '0'),
+('3', 'Restaurant O Brazil', 'contact@restaurant-obrazil-strasbourg.fr', 'notAPassword', '13 Rue du Jeu des Enfants', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('4', 'Fink Stuebel', 'contact@restaurant-finkstuebel.com', 'notAPassword', '26 Rue Finkwiller', 67000, 'Strasbourg', '2017-02-02 00:00:00', NULL, NULL, '1', '0'),
+('5', 'Le Tire-Bouchon', 'contact@letirebouchon.fr', 'notAPassword', '5 Rue des Tailleurs de Pierre', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('6', 'Saint-Sépulcre', 'contact@saintsepulcre.fr', 'notAPassword', '15 Rue des Orfèvres', 67000, 'Strasbourg', '2017-02-05 00:00:00', NULL, NULL, '1', '0'),
+('7', 'Cinnamon', 'contact@cinnamonrestaurant.fr', 'notAPassword', '15 Rue de la Division Leclerc', 67000, 'Strasbourg', '2017-01-08 00:00:00', NULL, NULL, '1', '0'),
+('8', 'Le New Capri', 'contact@newcapri.fr', 'notAPassword', '78 Avenue des Vosges', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('9', 'Velicious', 'contact@velicious.fr', 'notAPassword', '43 Rue Geiler', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('10', 'Le Pied de Mammouth', 'contact@lpmburger.fr', 'notAPassword', '58 Avenue de la Forêt-Noire', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('11', 'verrine-vapeur.com', 'contact@', 'notAPassword', '', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('12', 'Subway', 'contact@subwayfrance.fr', 'notAPassword', '18 Rond-Point de l\'Esplanade', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('13', 'Brasserie Les Haras', 'contact@les-haras-brasserie.com', 'notAPassword', '23 Rue des Glacières', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('14', 'La Ruche', 'contact@larucheauxdeuxreines.fr', 'notAPassword', '34 Rue de la Course', 67000, 'Strasbourg', '2017-01-22 00:00:00', NULL, NULL, '1', '0'),
+('15', 'L\'Épicerie', 'contact@lepicerie-strasbourg.com', 'notAPassword', '', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0');
+('16', 'Brasserie La Solidarité', 'contact@brasseriesolidarite.fr', 'notAPassword', '5 Rue du Travail', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0'),
+('17', 'Au Marché Gare', 'contact@marche-gare.com', 'notAPassword', '55 Rue du Marché Gare', 67000, 'Strasbourg', '2017-02-08 00:00:00', NULL, NULL, '1', '0');
